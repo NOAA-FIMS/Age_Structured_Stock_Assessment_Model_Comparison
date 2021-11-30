497783d5d6cac55e9b7f8aa178d85320733baa64
Matthew-Supernaw-NOAA <matthew.supernaw@noaa.gov>
2021-11-30 08:44:56 -0500

    Update plot_msy_re.R

--- R/plot_msy_re.R
+++ R/plot_msy_re.R
@@ -2,7 +2,7 @@
 plot_msy_re <- function(em_names, col, casedir=NULL){
   msy_re <- matrix(NA, ncol=length(em_names)*3, nrow=keep_sim_num)
   for(i in 1:keep_sim_num){
-    msy_re[i,]<-c(as.numeric(as.character(re_list[[i]]$msy)), as.numeric(as.character(re_list[[i]]$fmsy)), as.numeric(as.character(re_list[[i]]$ssbmsy)))
+    msy_re[i,]<-c(as.numeric(as.character(re_list[[i]]$msy)), as.numeric(as.character(re_list[[i]]$fmsy)), as.numeric(as.character(re_list[[i]]$Rmsy)),as.numeric(as.character(re_list[[i]]$ssbmsy)))
   }
 
   jpeg(file=file.path(casedir, "figure", "msy_re_sim.jpg"), width=150, height=120, units="mm", res=300)
